bbdfde5ce7ee6f7e35bfab2827130c889fa19d11
JC <jc@tpages.com>
2019-01-30 17:22:31 +0800

    add cors middleware

---
+++ src/Middleware/CorsMiddleware.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * MIT License
+ *
+ * Copyright (c) Jackey Cheung
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE
+ * SOFTWARE.
+ *
+ */
+
+namespace edign8\Laravel\Middleware;
+
+use Closure;
+
+class CorsMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure                 $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        /* @var \Illuminate\Http\Response $response */
+        $response = $next($request);
+
+        $response->header(
+            'access-control-allow-origin',
+            $request->header('origin')
+        );
+
+        $header = $request->header('access-control-request-method');
+        if ($header) {
+            $response->header('access-control-allow-methods', $header);
+        }
+
+        $header = $request->header('access-control-request-headers');
+        if ($header) {
+            if (is_array($header)) {
+                $header = implode(', ', $header);
+            }
+            $response->header('access-control-allow-headers', $header);
+        }
+
+        return $response;
+    }
+}
